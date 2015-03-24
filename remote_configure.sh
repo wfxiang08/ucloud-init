f07d20749447e6eb8110242ffdf2302220868220
Christophe Nick <nick@mobilecity.de>
2015-03-24 18:16:58 +0100

    Removed two commands

--- remote_configure.sh
+++ remote_configure.sh
@@ -1,9 +1,7 @@
 
-git clone https://github.com/Gingonic/ucloud-init.git && \
-   cd ucloud-init && \
-   ./build.sh && \
-   mkdir /opt/bin && \
-   cp binaries/* /opt/bin/ && \
-   ./configure $1 $2 $3
+./build.sh && \
+mkdir /opt/bin && \
+cp binaries/* /opt/bin/ && \
+./configure $1 $2 $3
 
 
